f70b402070dbee420a5efe14121036c196a0e890
Alex Haridis <charidis@mit.edu>
2023-01-31 12:55:51 -0500

    Added Data Arrays and D3

--- homework/01-31/scripts/myFunctions.js
+++ homework/01-31/scripts/myFunctions.js
@@ -1,9 +1,15 @@
 // A series of custom functions. This file can be considered
 // as a custom library, and each of the functions below can be
-// called from an html page.
+// called from an html page after this js file has been loaded.
 
 function sumAB(A, B) {
 
     return A + B;
 
+}
+
+function randomPosition() {
+
+    return Math.random() * 100 + 50;
+    
 }
