6b8272dafcb9f787160345f4a8a321088eb25e17
Alex Haridis <charidis@mit.edu>
2023-03-29 13:02:17 -0400

    Added Hierarchy sketch

---
+++ ARTG5430/Hierarchy/scripts/script.js
@@ -0,0 +1,103 @@
+// Define the dimensions of the space that will contain the hierarhy
+const width = 700;
+const height = 380;
+
+const svg = d3.select("#chart")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+d3.json("data/films.json").then(function(results){ 
+    createHierarchy(results);
+});
+
+//
+
+function createHierarchy(data) {
+
+    /**
+     *  The following lines of code handle the ABSTRACT DATA STRUCTURE
+     *  of a hierarchy. No visualization-specific methods are used.
+     */
+
+    let groups = d3.rollup(data, function(d) { return d.length; },
+                                 function(d) { return d.Distributor; },
+                                 function(d) { return d.Genre; }
+                                //  function(d) { return d.Rating; }
+    );
+
+    let root = d3.hierarchy(groups);
+
+    root.sum(function(d) {
+        return d[1];
+    });
+
+    let treeLayout = d3.tree()
+        .size([width, height - 40]);
+
+    treeLayout(root);
+
+    /**
+     *  The following lines of code handle VISUALIZATION-SPECIFIC methods.
+     */
+
+    // Append and Draw Links between Nodes
+    svg.append("g")
+        .selectAll('line')
+        .data(root.links())
+        .join('line')
+        // Coordinates of start of line
+        .attr('x1', function(d) {return d.source.x;})
+        .attr('y1', function(d) {return d.source.y;})
+        .style("stroke-width", 6)
+        // Coordinates of end of line
+        .attr('x2', function(d) {return d.target.x;})
+        .attr('y2', function(d) {return d.target.y;});
+
+    // Visualize the Nodes in the Hierarchy as circles
+    svg.append("g")
+        .selectAll('circle')
+        .data(root.descendants())
+        .join('circle')
+        .attr('cx', function(d) {return d.x;})
+        .attr('cy', function(d) {return d.y;})
+        .attr('r', 8);
+
+    // Draw Labels for Each Node/Circle
+    svg.append("g")
+        .selectAll('text.label')
+        .data(root.descendants())
+        .join('text')
+        .classed('label', true)
+        .attr('x', function(d) {return d.x;})
+        .attr('y', function(d) {return d.y - 15;})
+        .style("font-family", "Saira")
+        .style("font-size", "14px")
+        .style("font-weight", "bold")
+        .text(function(d) {
+            return d.data[0];
+        });
+
+    // Leaf count labels
+    svg.append("g")
+        .selectAll('text.count-label')
+        .data(root.descendants())
+        .join('text')
+        .classed('count-label', true)
+        .attr('x', function(d) {return d.x;})
+        .attr('y', function(d) {return d.y + 24;})
+        .style("font-family", "Saira")
+        .style("font-size", "14px")
+        .text(function(d) {
+            if (d.height > 0) return '';
+            return d.data[1];
+        });
+
+    // This is only for translating all "g" group elements downwards.
+    // Experiment with the values to observe how the tree layout is positioned
+    // within the allocated space given by the width and height variables.
+
+    d3.selectAll("g")
+        .attr("transform", `translate(0, 15)`);
+
+}
--- homework/03-28/scripts/script.js
+++ homework/03-28/scripts/script.js
@@ -1,241 +1,222 @@
-var url = "https://whiteboard.datawheel.us/api/google-analytics/realtime/random";
-var frequency = 1 * 1000;
-
-var margin = {
-    top: 20,
-    right: 100,
-    bottom: 100,
-    left: 40
-};
+var realtimeURL = "https://whiteboard.datawheel.us/api/google-analytics/realtime/random";
+var frequency = 1 * 1000; // 1 seconds
 
 var dataMax = 10;
 var data = [];
 
 var width = window.innerWidth;
 var height = window.innerHeight;
+
+var margin = {
+    top: 20,
+    right: 150,
+    bottom: 100,
+    left: 50
+};
+
 var chartWidth = width - margin.left - margin.right;
 var chartHeight = height - margin.top - margin.bottom;
+
+var svg = d3.select("#chart")
+.attr("width", width)
+.attr("height", height);
 
 var scaleWidth = 300;
 var scaleHeight = 20;
 var scaleX = margin.left + chartWidth / 2 - scaleWidth / 2;
 var scaleY = margin.top + chartHeight + 40;
 
-var svg = d3.select("#chart")
-            .attr("width", width)
-            .attr("height", height);
-
 var scale = svg.select("#scale")
-               .attr("transform", "translate(" + scaleX + ", " + scaleY + ")");
+.attr("transform", "translate(" + scaleX + ", " + scaleY + ")");
 
 scale.select("#scaleRect")
-     .attr("width", scaleWidth)
-     .attr("height", scaleHeight);
+.attr("width", scaleWidth)
+.attr("height", scaleHeight);
 
 var legendX = margin.left + chartWidth;
 var legendY = margin.top;
 var legendSize = 20;
+var legendPadding = 10;
 
 var legend = svg.select("#legend")
-                .attr("transform", "translate(" + legendX + ", " + legendY + ")");
+.attr("transform", "translate(" + legendX + ", " + legendY + ")");
 
-var dataRange = d3.range(1, dataMax + 1);
+var domainValues = d3.range(1, dataMax + 1);
 
 var x = d3.scaleBand()
-          .domain(dataRange.reverse())
-          .range([margin.left, margin.left + chartWidth])
-          .paddingInner(0.1)
-          .paddingOuter(0.2);
+.domain(domainValues.reverse())
+.range([margin.left, margin.left + chartWidth])
+.paddingInner(0.2)
+.paddingOuter(0.1);
 
 var barWidth = x.bandwidth();
 
 function fetchData() {
-    d3.json(url, function(error, users) {
 
-        data.unshift({
-            users: users,
-            timestamp: new Date()
-        });
+d3.json(realtimeURL).then(function(error, users) {
 
-        if (data.length > dataMax) data.pop();
-        console.log(data);
+    var dataObject = {
+    users: users,
+    timestamp: new Date()
+    };
 
-        if (data.length === dataMax) clearInterval(myInterval);
+    data.unshift(dataObject);
+    if (data.length > dataMax) data.pop();
+    if (data.length === dataMax) clearInterval(myInterval);
 
-        var maxUsers = d3.max(data, function(d) {
-            return d.users;
-        });
+    var legendData = data.map(function(d) {
+    return d.users;
+    });
 
-        var minUsers = d3.min(data, function(d) {
-            return d.users;
-        });
+    legendData = legendData.filter(function(d, i) {
+    return legendData.indexOf(d) === i;
+    });
 
-        // Color Scale Stuff
-        var barColor = d3.scaleSequential(d3.interpolateViridis)
-                        .domain([0, maxUsers]);
+    legendData = legendData.sort(function(a, b) {
+    return b - a;
+    });
 
-        var stopsData = d3.range(0, 1.1, 0.1);
+    var maximum = d3.max(data, function(d) {
+    return d.users;
+    });
 
-        svg.select("#colorGradient").selectAll("stop")
-        .data(stopsData).enter()
-        .append("stop")
+    var barColor = d3.scaleSequential(d3.interpolatePlasma)
+    .domain([0, maximum]);
+
+    var legendRects = legend.selectAll("rect")
+    .data(legendData);
+
+    var legendRectsEnter = legendRects.enter().append("rect");
+
+    legendRects.merge(legendRectsEnter)
+    .attr("x", legendPadding)
+    .attr("y", function(d, i) {
+        return i * legendSize + i * legendPadding;
+    })
+    .attr("width", legendSize)
+    .attr("height", legendSize)
+    .attr("fill", barColor);
+
+    var legendTexts = legend.selectAll("text")
+    .data(legendData);
+
+    var legendTextsEnter = legendTexts.enter().append("text")
+    .attr("baseline-shift", "-100%");
+
+    legendTexts.merge(legendTextsEnter)
+    .attr("x", legendPadding + legendSize + legendPadding / 2)
+    .attr("y", function(d, i) {
+        return i * legendSize + i * legendPadding;
+    })
+    .text(function(d) {
+        return d;
+    });
+
+    var stops = d3.range(0, 1.25, 0.25);
+
+    svg.select("#colorGradient").selectAll("stop")
+    .data(stops).enter().append("stop")
         .attr("offset", function(d) {
-                return d * 100 + "%";
+        return d * 100 + "%";
         })
         .attr("stop-color", function(d) {
-                return barColor(d * maxUsers);
+        return barColor(d * maximum);
         });
 
-        var scaleX = d3.scaleLinear()
-            .domain([0, maxUsers])
-            .range([0, scaleWidth]);
+    var gradientScale = d3.scaleLinear()
+        .domain([0, maximum])
+        .range([0, scaleWidth]);
 
-        var scaleAxis = d3.axisBottom(scaleX);
+    var gradientAxis = d3.axisBottom(gradientScale);
 
-        scale.select("#scaleAxis")
-            .attr("transform", "translate(0, " + scaleHeight + ")")
-            .transition().duration(frequency / 2)
-            .call(scaleAxis);
-
-        // Color Legend Stuff
-        var legendData = data.map(function(d) {
-            return d.users;
-        });
-
-        legendData = legendData
-            .filter(function(d, i) {
-                return legendData.indexOf(d) === i;
-            })
-            .sort(function(a, b) {
-                return b - a;
-            });
-
-        var legendRects = legend.selectAll("rect")
-                                .data(legendData);
-
-        var legendRectsEnter = legendRects.enter().append("rect");
-
-        legendRects.merge(legendRectsEnter)
-                .attr("x", 0)
-                .attr("y", function(d, i) {
-                    return i * legendSize + i * 10;
-                })
-                .attr("fill", barColor)
-                .attr("width", legendSize)
-                .attr("height", legendSize);
-
-        var legendTexts = legend.selectAll("text")
-                                .data(legendData);
-
-        var legendTextsEnter = legendTexts.enter().append("text")
-                                        .attr("baseline-shift", "-100%");
-
-        legendTexts.merge(legendTextsEnter)
-                .attr("x", legendSize + 5)
-                .attr("y", function(d, i) {
-                        return i * legendSize + i * 10;
-                })
-                .text(function(d) {
-                        return d;
-                });
-
-        // Bar Chart Stuff
-        var barHeight = d3.scaleLinear()
-                        .domain([0, maxUsers])
-                        .range([0, chartHeight]);
-
-        var y = d3.scaleLinear()
-                .domain([0, maxUsers])
-                .range([margin.top + chartHeight, margin.top]);
-
-        var yAxis = d3.axisLeft(y);
-
-        svg.select("#y")
-        .attr("transform", "translate(" + margin.left + ",0)")
+    scale.select("#scaleAxis")
+        .attr("transform", "translate(0, " + scaleHeight + ")")
         .transition().duration(frequency / 2)
-        .call(yAxis);
+        .call(gradientAxis);
 
-        var xAxis = d3.axisBottom(x)
-        .tickFormat(function(d) {
+    var barHeight = d3.scaleLinear()
+    .domain([0, maximum])
+    .range([0, chartHeight]);
 
-            var dataPoint = data[d - 1];
-            if (dataPoint) {
-                var time = dataPoint.timestamp;
-                var diff = new Date() - time;
-                var secondsAgo = Math.round(diff / 1000);
-                
-                if (secondsAgo == 0) return "Now";
-                    else {
-                        var word = secondsAgo == 1 ? "second" : "seconds";
-                        return secondsAgo + " " + word + " ago";
-                    }
-            }
-            else {
-                return "";
-            }
+    var y = d3.scaleLinear()
+    .domain([0, maximum])
+    .range([margin.top + chartHeight, margin.top]);
 
-        });
+    var yAxis = d3.axisLeft(y);
+    svg.select("#y")
+    .attr("transform", "translate(" + margin.left + ", 0)")
+    .transition().duration(frequency / 2)
+    .call(yAxis);
 
-        svg.select("#x")
-        .attr("transform", "translate(0," + y(0) + ")")
-        .transition().duration(frequency / 2)
-        .call(xAxis);
-
-        function zeroState(selection) {
-            selection
-                .attr("height", 0)
-                .attr("y", y(0));
+    var xAxis = d3.axisBottom(x)
+    .tickFormat(function(d) {
+        var tickData = data[d - 1];
+        if (tickData) {
+        var now = new Date();
+        var msAgo = now - tickData.timestamp;
+        var secondsAgo = Math.round(msAgo / 1000);
+        if (secondsAgo === 0) {
+            return "Now";
         }
-
-        // Bar data binding
-        var bars = svg.select("#shapes").selectAll(".bar")
-                    .data(data, function(d) {
-                        return d.timestamp;
-                    });
-
-        // Bar "enter" events and animation
-        bars.enter().append("rect")
-            .attr("class", "bar")
-            .call(zeroState)
-            .attr("width", barWidth)
-            .attr("x", function(d, i) {
-                return x(i + 1);
-            })
-            .attr("fill", function(d) {
-                return barColor(d.users);
-            })
-            .transition().duration(frequency / 2)
-            .attr("height", function(d) {
-                return barHeight(d.users);
-            })
-            .attr("y", function(d) {
-                return y(d.users);
-            });
-
-        // Bar "update" events and animation
-        bars.transition().duration(frequency / 2)
-        .attr("fill", function(d) {
-            return barColor(d.users);
-        })
-        .attr("height", function(d) {
-            return barHeight(d.users);
-        })
-        .attr("x", function(d, i) {
-            return x(i + 1);
-        })
-        .attr("y", function(d) {
-            return y(d.users);
-        });
-
-        // Bar "exit" events and animation
-        bars.exit()
-            .transition().duration(frequency / 2)
-            .call(zeroState)
-            .remove();
-
+        else {
+            var word = secondsAgo === 1 ? "second" : "seconds";
+            return secondsAgo + " " + word + " ago";
+        }
+        }
+        else {
+        return "";
+        }
     });
+
+    svg.select("#x")
+    .attr("transform", "translate(0, " + (margin.top + chartHeight) + ")")
+    .call(xAxis);
+
+    var bars = svg.select("#shapes").selectAll(".bar")
+    .data(data, function(d) {
+        return d.timestamp;
+    });
+
+    function zeroState(selection) {
+    selection
+        .attr("height", 0)
+        .attr("y", function(d) {
+        return y(d.users);
+        });
+    }
+
+    var enter = bars.enter().append("rect")
+    .attr("class", "bar")
+    .attr("width", barWidth)
+    .call(zeroState)
+    .attr("fill", "transparent")
+    .attr("x", function(d, i) {
+        return x(i + 1);
+    });
+
+    bars.merge(enter)
+    .transition().duration(frequency / 2)
+    .attr("fill", function(d) {
+        return barColor(d.users);
+    })
+    .attr("height", function(d) {
+        return barHeight(d.users);
+    })
+    .attr("y", function(d) {
+        return y(d.users);
+    })
+    .attr("x", function(d, i) {
+        return x(i + 1);
+    });
+
+    bars.exit()
+    .transition().duration(frequency / 2)
+    .call(zeroState)
+    .remove();
+
+});
+
 }
 
 fetchData();
-
 var myInterval = setInterval(fetchData, frequency);
